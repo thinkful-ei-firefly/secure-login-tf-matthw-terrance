d71015afedcd3f63328396d04e8ae9f03d0c1375
Matthew Mbonu <mcmbonu1@gmail.com>
2019-09-05 14:50:29 -0400

    fixed db bug

--- thingful-client/src/components/LoginForm/LoginForm.js
+++ thingful-client/src/components/LoginForm/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import TokenService from "../../services/token-service";
 import { Button, Input } from "../Utils/Utils";
-
+import AuthApiService from "../../services/auth-api-service";
 export default class LoginForm extends Component {
   static defaultProps = {
     onLoginSuccess: () => {}
